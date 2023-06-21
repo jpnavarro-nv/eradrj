e6aa25ab60b48866d54ee8b28363253827496da3
João Paulo Navarro <jpnavarro@nvidia.com>
2023-06-21 15:18:51 -0300

    Adjust make.

--- hackathon/make_run_serial.sh
+++ hackathon/make_run_serial.sh
@@ -9,4 +9,4 @@
 #SBATCH --partition=debug
 #SBATCH --output=make_run_%j.log
 
-NVARCH=`uname -s`_`uname -m`; export NVARCH && NVCOMPILERS=/opt/nvidia/hpc_sdk; export NVCOMPILERS && MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/23.5/compilers/man; export MANPATH && PATH=$NVCOMPILERS/$NVARCH/23.5/compilers/bin:$PATH; export PATH && && LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvidia/hpc_sdk/Linux_x86_64/23.5/cuda/12.1/lib64/; export LD_LIBRARY_PATH && cd n_body/serial && make && ./rdf
+NVARCH=`uname -s`_`uname -m`; export NVARCH && NVCOMPILERS=/opt/nvidia/hpc_sdk; export NVCOMPILERS && MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/23.5/compilers/man; export MANPATH && PATH=$NVCOMPILERS/$NVARCH/23.5/compilers/bin:$PATH; export PATH && LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvidia/hpc_sdk/Linux_x86_64/23.5/cuda/12.1/lib64/; export LD_LIBRARY_PATH && cd n_body/serial && make && ./rdf
