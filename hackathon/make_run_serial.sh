e0aed913fc0080cc95ce9f1f84fea9eb3dcdd338
João Paulo Navarro <jpnavarro@nvidia.com>
2023-06-21 15:08:22 -0300

    Make run submit script.

--- hackathon/make_run_serial.sh
+++ hackathon/make_run_serial.sh
@@ -15,4 +15,4 @@ MANPATH=$MANPATH:$NVCOMPILERS/$NVARCH/23.5/compilers/man; export MANPATH
 PATH=$NVCOMPILERS/$NVARCH/23.5/compilers/bin:$PATH; export PATH
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvidia/hpc_sdk/Linux_x86_64/23.5/cuda/12.1/lib64/; export LD_LIBRARY_PATH
 
-cd n_body/serial && make && rdf
+cd n_body/serial && make && ./rdf
